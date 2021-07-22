37114de3c2f822d7c2045469c68efb88fa84fbc3
Amaka-A <77367947+Amaka-A@users.noreply.github.com>
2021-07-22 18:12:44 -0500

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -35,7 +35,7 @@ function send() {
     console.log(remove_charAt3);
 
     question_word = "<h4 id='word_display'> Q. " + remove_charAt3 + "</h4>";
-    input_box = "<br>Answer : <input type='text='input_check_box'>";
+    input_box = "<br>Answer : <input type='text'id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row = question_word + input_box + check_button;
     document.getElementById("output").innerHTML = row;
